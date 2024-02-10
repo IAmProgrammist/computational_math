4d73ecd20a0e19910ba1ec6711b0f6e7054ab735
IAmProgrammist <vladislav.pakhomov@bk.ru>
2024-02-10 17:23:38 +0300

    Found a working example

--- src/lab1/task4/main.rs
+++ src/lab1/task4/main.rs
@@ -4,8 +4,8 @@ use algr::lab1::task4::{make_computation, make_improved_computation};
 
 fn main() {
     let pri = 1.0f32;
-    let rat = 132.3112;
-    let per = 3.0;
+    let rat = -0.9999f32;
+    let per = 4.0f32;
 
     println!("{:.32}", make_computation(pri, rat, per));
     println!("{:.32}", make_improved_computation(pri, rat, per));
